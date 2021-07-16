65882769ee47654aef204552e38f3a879d4f38be
Zachary Nicolaou <zack.nicolaou@gmail.com>
2021-07-15 22:18:47 -0700

    updated encode
    Former-commit-id: 61d3af98ee6324d5b22cfd1ac7d1a418cfaa4a91

--- encode.sh
+++ encode.sh
@@ -2,5 +2,5 @@
 if [ $# -ne 3 ]; then
   echo usage: encode.sh infolder outfile rate
 else
-  ffmpeg -y -r $3 -i $1/%04d.png -c:v libx264 -preset fast -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2,format=yuv420p" $2.mp4
+  ffmpeg -hide_banner -loglevel error -y -r $3 -i $1/%04d.png -c:v libx264 -preset fast -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2,format=yuv420p" $2.mp4
 fi
